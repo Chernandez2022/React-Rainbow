1d63479770344fa3c87b973b83206cba712bac28
Chernandez2022 <hcarlos0816@gmail.com>
2022-09-24 00:58:19 -0400

    inital commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import ColorBlock from "./ColorBlock";
+import ColorForm from "./ColorForm";
+import "./App.css";
+
+function App() {
+  let [colors, setColors] = useState([
+    "violet",
+    "blue",
+    "lightblue",
+    "green",
+    "greenyellow",
+    "yellow",
+    "orange",
+    "red",
+  ]);
+
+  let colorMap = colors.map((color, i) => {
+    return <ColorBlock key={`${color}_${i}`} color={color} />;
+  });
+
+  const addColor = (newColor) => {
+    setColors([...colors, newColor]);
+  };
+
+  return (
+    <div className="App">
+      {colorMap}
+      <ColorForm addColor={addColor} />
+    </div>
+  );
+}
+
+export default App;
+// function App() {
+//   return (
+//     <div className="App">
+
+//     </div>
+//   );
+// }
+
+// export default App;
